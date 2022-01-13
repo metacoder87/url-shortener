cd67a9b4e40212380c7f6fd3a8419d41db5049f1
metacoder87 <gary.miller@gmx.com>
2022-01-13 16:48:28 -0500

    Adds Visit::record_visit!

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -8,4 +8,8 @@ class Visit < ApplicationRecord
 
     belongs_to :shortened_url
 
+    def self.record_visit!(user, shortened_url)
+        Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+    end
+
 end
