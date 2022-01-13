e7a8d73aa0da78bdf43a38081f19f488de89440f
metacoder87 <gary.miller@gmx.com>
2022-01-12 23:36:11 -0500

    Adds User association to submitter urls

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true
+
+    has_many :submitter_urls,
+        primary_key: :id,
+        class_name: :ShortenedUrl,
+        foreign_key: :submitter_id
 end
