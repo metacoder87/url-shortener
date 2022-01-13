5d1942094c7d499ae3abf9759cc051e7f9e3f6f4
metacoder87 <gary.miller@gmx.com>
2022-01-12 20:09:43 -0500

    Adds ShortenedUrl class to model

---
+++ app/models/shortened_url.rb
@@ -0,0 +1,16 @@
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, :long_url, :submitter, presence: true
+    validates :short_url, uniqueness: true
+
+    belongs_to :submitter,
+        primary_key: :id,
+        class_name: :User,
+        foreign_key: :submitter_id
+
+    def self.random_code
+        loop do
+            rando = SecureRandom.urlsafe_base64(16)
+            return rando unless ApplicationRecord.exists?(short_url: rando)
+        end
+    end
+end
