f4193bf860dcb56cf3602cca320cd656bf896bae
metacoder87 <gary.miller@gmx.com>
2022-01-13 16:21:16 -0500

    Fixes User model naming error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true
 
-    has_many :submitter_urls,
+    has_many :submitted_urls,
         primary_key: :id,
         class_name: :ShortenedUrl,
         foreign_key: :submitter_id
