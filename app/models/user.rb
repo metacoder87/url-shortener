4bbde3dc73c4a3a664298e0fbf673ec8f73ab3cf
metacoder87 <gary.miller@gmx.com>
2022-01-13 16:54:11 -0500

    Adds Visitor associations to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
         primary_key: :id,
         class_name: :ShortenedUrl,
         foreign_key: :submitter_id
+
+    has_many :visits,
+        primary_key: :id,
+        class_name: :Visit,
+        foreign_key: :user_id
+
+    has_many :visited_urls,
+        through: :visits,
+        source: :shortened_url
 end
