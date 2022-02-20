ebfd189ae56f516ecd6f96ee1340581b2311d18f
metacoder87 <gary.miller@gmx.com>
2022-02-20 01:03:51 -0500

    Updates the db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220130025847) do
+ActiveRecord::Schema.define(version: 20220220052746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20220130025847) do
     t.datetime "updated_at", null: false
     t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
     t.index ["submitter_id"], name: "index_shortened_urls_on_submitter_id"
+  end
+
+  create_table "tag_topics", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taggings", force: :cascade do |t|
