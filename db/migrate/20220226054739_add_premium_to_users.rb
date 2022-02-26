2159561ad8ffb7fba59c8124efd0bfd61028f612
metacoder87 <gary.miller@gmx.com>
2022-02-26 16:50:12 -0500

    Fixes update to users table

--- db/migrate/20220226054739_add_premium_to_users.rb
+++ db/migrate/20220226054739_add_premium_to_users.rb
@@ -1,5 +1,5 @@
 class AddPremiumToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column(:users, :premium, :boolean, {null: false})
+    add_column :users, :premium, :boolean, :default => false
   end
 end
